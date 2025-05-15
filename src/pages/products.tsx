676b3cf79b1fc6fc1d3ff884fcadedeb1939eac8
Sandesh Sapkota <sapkotasandesh88@gmail.com>
2025-05-15 22:55:21 +0545

    experiments and toast implementation

---
+++ src/pages/products.tsx
@@ -0,0 +1,15 @@
+import RootLayout from "@/components/layout/layout";
+import Sections from "@/components/home/section";
+
+const About = () => {
+
+    return (
+        <RootLayout>
+            <div className="container min-h-container">
+                <Sections/>
+            </div>
+        </RootLayout>
+    )
+}
+
+export default About
